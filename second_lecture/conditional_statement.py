88db3d07650026105b36545da09e3de6b1be1fed
granthsoni525 <granthsoni5@gmail.com>
2026-06-30 20:12:38 +0530

    third_lecture

--- second_lecture/conditional_statement.py
+++ second_lecture/conditional_statement.py
@@ -5,11 +5,30 @@
 
 
 
-light = "green"
+# light = "pink"
 
-if(light== "red"):
-    print("stop")
-elif(light == "green"):
-    print("go")
-elif(light == "yellow"):
-    print("wait")
+# if(light== "red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("wait")
+# else:
+#     print("light is broken")
+
+# print("end of code")
+
+marks = int(input("enter student marks: "))
+
+if(marks >= 90):
+    print("Grade A")
+elif(marks >= 80):
+    print("Grade B")
+elif(marks >= 70):
+    print("Grade C")
+elif(marks >= 60):
+    print("Grade D")
+else:
+    print("Grade F")
+
+print("grade of the student -->", marks)
